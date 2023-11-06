a2a5674f31f8fbc0378e14d777f117a75fbf29cb
tushar-454 <imtushar454@gmail.com>
2023-11-06 19:29:02 +0600

    single a service booked  ui and post data function added successfully

--- src/Components/SingleService/SingleService.jsx
+++ src/Components/SingleService/SingleService.jsx
@@ -1,12 +1,18 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import swal from 'sweetalert';
 import Container from '../Reusable/Container';
+import Button from '../UI/Button';
+import Input from '../UI/Input';
 import classes from './SingleService.module.css';
 
 const SingleService = () => {
   const [service, setService] = useState({});
   const { id } = useParams();
+  const [modalShow, setModalShow] = useState(false);
+  const [bookingData, setBookingData] = useState({ date: '', instruction: '' });
+  const [error, setError] = useState({ date: '' });
   const {
     image,
     name,
@@ -15,7 +21,43 @@ const SingleService = () => {
     servicearea,
     authorImage,
     authorName,
+    email,
   } = service;
+  // handle input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setBookingData((prevObj) => ({ ...prevObj, [name]: value }));
+    setError({ date: '' });
+  };
+  // handle booking services
+  const handleBookingService = (e) => {
+    e.preventDefault();
+    const { date, instruction } = bookingData;
+    if (!date) {
+      setError((prevError) => ({ ...prevError, date: 'Date require !' }));
+      return;
+    }
+    const bookedData = {
+      image,
+      name,
+      description,
+      price,
+      servicearea,
+      authorImage,
+      authorName,
+      email,
+      date,
+      instruction,
+    };
+    axios
+      .post('/booking', bookedData)
+      .then((res) => {
+        if (res.data.acknowledged) {
+          swal('Service Booked', '', 'suceess');
+        }
+      })
+      .catch((error) => swal('There was an error !', error.message, 'error'));
+  };
   useEffect(() => {
     axios(`/service/${id}`).then((res) => setService(res.data[0]));
   }, [id]);
@@ -46,10 +88,68 @@ const SingleService = () => {
               <h3>{authorName}</h3>
             </div>
             <div className={classes.viewMore}>
-              <button>Book Now</button>
+              <button onClick={() => setModalShow(true)}>Book Now</button>
             </div>
           </div>
         </div>
+        {modalShow && (
+          <div className={classes.bookingModalWrap}>
+            <button
+              className={classes.modalHideBtn}
+              onClick={() => setModalShow(false)}
+            >
+              X
+            </button>
+            <div className={classes.bookingModal}>
+              <form onSubmit={handleBookingService}>
+                <div className={classes.inputGroup}>
+                  <Input
+                    displayName={'Service Name'}
+                    defaultValue={name}
+                    readOnly
+                  />
+                  <div className={classes.modalImage}>
+                    <img src={image} />
+                  </div>
+                </div>
+                <div className={classes.inputGroup}>
+                  <Input
+                    displayName={'Service Provider Email'}
+                    defaultValue={email}
+                    readOnly
+                  />
+                  <Input
+                    displayName={'Service Provider Name'}
+                    defaultValue={authorName}
+                    readOnly
+                  />
+                </div>
+                <div className={classes.inputGroup}>
+                  <Input
+                    displayName={'Service Taking Date'}
+                    type={'date'}
+                    name={'date'}
+                    value={bookingData.date}
+                    onChange={handleInputChange}
+                    error={error.date}
+                  />
+                  <Input displayName={'Price'} defaultValue={price} readOnly />
+                </div>
+                <Input
+                  displayName={'Special Instruction'}
+                  type={'text'}
+                  placeholder={
+                    'give your any instruction or customize service you can write here'
+                  }
+                  name={'instruction'}
+                  value={bookingData.instruction}
+                  onChange={handleInputChange}
+                />
+                <Button displayName={'Purchase this Service'} type={'submit'} />
+              </form>
+            </div>
+          </div>
+        )}
       </Container>
     </section>
   );

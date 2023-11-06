37ffef3b86adca8276ca2bec99df431273d140c1
tushar-454 <imtushar454@gmail.com>
2023-11-06 22:02:39 +0600

    if user have no any added services show a message added

--- src/Components/ManageServices/ManageServices.jsx
+++ src/Components/ManageServices/ManageServices.jsx
@@ -7,6 +7,7 @@ import classes from './ManageServices.module.css';
 
 const ManageServices = () => {
   const [userAllServices, setUserAllServices] = useState([]);
+  const [showingUserServices, setShowingUserServices] = useState([]);
   const { user } = useContext(AuthContext);
 
   // handle user added service delete
@@ -26,6 +27,8 @@ const ManageServices = () => {
               'Your added services deleted successfully',
               'success'
             );
+            const remaining = userAllServices.filter((item) => item._id !== id);
+            setShowingUserServices(remaining);
           } else {
             swal('There was an error !', '', 'error');
           }
@@ -37,9 +40,10 @@ const ManageServices = () => {
   };
 
   useEffect(() => {
-    axios
-      .get(`/services?email=${user.email}`)
-      .then((res) => setUserAllServices(res.data));
+    axios.get(`/services?email=${user.email}`).then((res) => {
+      setUserAllServices(res.data);
+      setShowingUserServices(res.data);
+    });
   }, [user.email]);
   return (
     <section>
@@ -47,36 +51,42 @@ const ManageServices = () => {
         <div className={classes.sectionTitle}>
           <h1>Manage your Services</h1>
         </div>
+        {showingUserServices.length === 0 && (
+          <div className={classes.noFound}>
+            <p>You have not added any services</p>
+          </div>
+        )}
         <div className={classes.manageServicesWrap}>
-          {userAllServices?.map((item) => (
-            <div key={item._id} className={classes.manageServicesItem}>
-              <img src={item.image} alt='services img' />
-              <div className={classes.manageServicesItemContent}>
-                <h1>{item.name}</h1>
+          {showingUserServices.length > 0 &&
+            showingUserServices?.map((item) => (
+              <div key={item._id} className={classes.manageServicesItem}>
+                <img src={item.image} alt='services img' />
+                <div className={classes.manageServicesItemContent}>
+                  <h1>{item.name}</h1>
 
-                <p>
-                  <b>Description</b>
-                  {item.description}
-                </p>
-                <p>
-                  <b>Price:</b> {item.price}
-                </p>
-              </div>
+                  <p>
+                    <b>Description</b>
+                    {item.description}
+                  </p>
+                  <p>
+                    <b>Price:</b> {item.price}
+                  </p>
+                </div>
 
-              <div className={classes.servicesAuthorWrap}>
-                <div className={classes.servicesAuthor}>
-                  <img src={item.authorImage} alt='authorimg' />
-                  <h3>{item.authorName}</h3>
-                </div>
-                <div className={classes.viewMore}>
-                  <button>Update</button>{' '}
-                  <button onClick={() => handleDeleteAddService(item._id)}>
-                    Delete
-                  </button>
+                <div className={classes.servicesAuthorWrap}>
+                  <div className={classes.servicesAuthor}>
+                    <img src={item.authorImage} alt='authorimg' />
+                    <h3>{item.authorName}</h3>
+                  </div>
+                  <div className={classes.viewMore}>
+                    <button>Update</button>{' '}
+                    <button onClick={() => handleDeleteAddService(item._id)}>
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </Container>
     </section>

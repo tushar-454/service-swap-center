5aee324ee5feab7513c57b18d89a8b81e824aa83
tushar-454 <imtushar454@gmail.com>
2023-11-06 23:02:26 +0600

    manage user added service means data update function added

--- src/Components/ManageServices/ManageServices.jsx
+++ src/Components/ManageServices/ManageServices.jsx
@@ -3,12 +3,18 @@ import { useContext, useEffect, useState } from 'react';
 import swal from 'sweetalert';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 import Container from '../Reusable/Container';
+import Button from '../UI/Button';
+import Input from '../UI/Input';
 import classes from './ManageServices.module.css';
 
 const ManageServices = () => {
   const [userAllServices, setUserAllServices] = useState([]);
   const [showingUserServices, setShowingUserServices] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
   const { user } = useContext(AuthContext);
+  const [willUpdate, setWillUpadate] = useState({});
+  const [willUpdateId, setWillUpdateId] = useState('');
+  const [fetch, setFetch] = useState(false);
 
   // handle user added service delete
   const handleDeleteAddService = (id) => {
@@ -38,13 +44,45 @@ const ManageServices = () => {
       }
     });
   };
+  // handle user added sevice update
+  const handleUpdateAddServices = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.serviceName.value;
+    const image = form.servicePhotoUrl.value;
+    const authorName = form.authorName.value;
+    const price = form.price.value;
+    const servicearea = form.area.value;
+    const description = form.desc.value;
+    const updatedServicesData = {
+      name,
+      image,
+      authorImage: willUpdate.authorImage,
+      authorName,
+      price,
+      servicearea,
+      description,
+      email: willUpdate.email,
+    };
+    axios.put(`/service/${willUpdateId}`, updatedServicesData).then((res) => {
+      if (res.data.modifiedCount === 1) {
+        swal('Services Data Updated', '', 'success');
+        setFetch(!fetch);
+      }
+    });
+  };
 
   useEffect(() => {
     axios.get(`/services?email=${user.email}`).then((res) => {
       setUserAllServices(res.data);
       setShowingUserServices(res.data);
     });
-  }, [user.email]);
+  }, [user.email, fetch]);
+  useEffect(() => {
+    axios
+      .get(`/service/${willUpdateId}`)
+      .then((res) => setWillUpadate(res.data[0]));
+  }, [willUpdateId, fetch]);
   return (
     <section>
       <Container>
@@ -79,7 +117,14 @@ const ManageServices = () => {
                     <h3>{item.authorName}</h3>
                   </div>
                   <div className={classes.viewMore}>
-                    <button>Update</button>{' '}
+                    <button
+                      onClick={() => {
+                        setWillUpdateId(item._id);
+                        setModalShow(true);
+                      }}
+                    >
+                      Update
+                    </button>{' '}
                     <button onClick={() => handleDeleteAddService(item._id)}>
                       Delete
                     </button>
@@ -89,6 +134,68 @@ const ManageServices = () => {
             ))}
         </div>
       </Container>
+      {modalShow && (
+        <div className={classes.bookingModalWrap}>
+          <button
+            className={classes.modalHideBtn}
+            onClick={() => setModalShow(false)}
+          >
+            X
+          </button>
+          <div className={classes.bookingModal}>
+            <form onSubmit={handleUpdateAddServices}>
+              <div className={classes.inputGroup}>
+                <Input
+                  displayName={'Service Name'}
+                  defaultValue={willUpdate.name}
+                  name={'serviceName'}
+                  type={'text'}
+                />
+                <Input
+                  displayName={'Service PhotoUrl'}
+                  defaultValue={willUpdate.image}
+                  name={'servicePhotoUrl'}
+                  type={'link'}
+                />
+              </div>
+              <div className={classes.inputGroup}>
+                <Input
+                  displayName={'Service Provider Email'}
+                  defaultValue={willUpdate.email}
+                  readOnly
+                />
+                <Input
+                  displayName={'Service Provider Name'}
+                  defaultValue={willUpdate.authorName}
+                  name={'authorName'}
+                  type={'text'}
+                />
+              </div>
+              <div className={classes.inputGroup}>
+                <Input
+                  displayName={'Price'}
+                  defaultValue={willUpdate.price}
+                  name={'price'}
+                  type={'number'}
+                />
+                <Input
+                  displayName={'Services Area'}
+                  defaultValue={willUpdate.servicearea}
+                  name={'area'}
+                  type={'text'}
+                />
+              </div>
+              <Input
+                displayName={'Description'}
+                defaultValue={willUpdate.description}
+                name={'desc'}
+                type={'text'}
+              />
+              <Button displayName={'Update Service'} type={'submit'} />
+            </form>
+          </div>
+        </div>
+      )}
     </section>
   );
 };

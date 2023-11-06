d174ad3d1936a5d12e53b04b51969d3ec660a6ed
tushar-454 <imtushar454@gmail.com>
2023-11-06 11:48:17 +0600

    slider fully completed fully raw hand coded

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -2,30 +2,20 @@ import { useEffect } from 'react';
 import sliderOneImg from '../../../assets/services1.jpg';
 import sliderTwoImg from '../../../assets/services2.jpg';
 import sliderThreeImg from '../../../assets/services3.jpg';
+import sliderFourImg from '../../../assets/services4.jpg';
+import sliderFiveImg from '../../../assets/services5.jpg';
+import sliderSixImg from '../../../assets/services6.jpg';
+import sliderSevenImg from '../../../assets/services7.jpg';
 // import Container from '../../Reusable/Container';
 import { useState } from 'react';
 import classes from './Banner.module.css';
 
 const Banner = () => {
   const [counter, setCounter] = useState(0);
-  const [slideLength, setSlideLength] = useState(0);
-  const nextSlide = () => {
-    if (counter >= slideLength - 1) {
-      setCounter(0);
-    } else {
-      setCounter((prevCount) => prevCount + 1);
-    }
-  };
-  const prevSlide = () => {
-    if (counter <= 0) {
-      setCounter(slideLength - 1);
-    } else {
-      setCounter((prevCount) => prevCount - 1);
-    }
-  };
+  const [buttons, setButtons] = useState([]);
   useEffect(() => {
     const slidesNode = document.querySelectorAll('.allSlider');
-    setSlideLength(slidesNode.length);
+    setButtons([...new Array(slidesNode.length).keys()]);
     slidesNode.forEach((slide, index) => {
       slide.style.left = `${index * 100}%`;
     });
@@ -45,7 +35,7 @@ const Banner = () => {
             <img src={sliderOneImg} />
             <div className={classes.sideContentWrap}>
               <div className={classes.slideContent}>
-                <h1>Affordable Price For Car Servicing</h1>
+                <h1>Ride Sharing</h1>
                 <p>
                   There are many variations of passages of available, but the
                   majority have suffered alteration in some form
@@ -58,7 +48,7 @@ const Banner = () => {
             <img src={sliderTwoImg} />
             <div className={classes.sideContentWrap}>
               <div className={classes.slideContent}>
-                <h1>Affordable Price For Car Servicing</h1>
+                <h1>Tool Sharing</h1>
                 <p>
                   There are many variations of passages of available, but the
                   majority have suffered alteration in some form
@@ -71,7 +61,59 @@ const Banner = () => {
             <img src={sliderThreeImg} />
             <div className={classes.sideContentWrap}>
               <div className={classes.slideContent}>
-                <h1>Affordable Price For Car Servicing</h1>
+                <h1>All Home Services</h1>
+                <p>
+                  There are many variations of passages of available, but the
+                  majority have suffered alteration in some form
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* slider - 4  */}
+          <div className={`${classes.slide} allSlider`}>
+            <img src={sliderFourImg} />
+            <div className={classes.sideContentWrap}>
+              <div className={classes.slideContent}>
+                <h1>Child co-oparative Service</h1>
+                <p>
+                  There are many variations of passages of available, but the
+                  majority have suffered alteration in some form
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* slider - 5  */}
+          <div className={`${classes.slide} allSlider`}>
+            <img src={sliderFiveImg} />
+            <div className={classes.sideContentWrap}>
+              <div className={classes.slideContent}>
+                <h1>Pet Sitting and Walking</h1>
+                <p>
+                  There are many variations of passages of available, but the
+                  majority have suffered alteration in some form
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* slider - 6  */}
+          <div className={`${classes.slide} allSlider`}>
+            <img src={sliderSixImg} />
+            <div className={classes.sideContentWrap}>
+              <div className={classes.slideContent}>
+                <h1>Book Swap</h1>
+                <p>
+                  There are many variations of passages of available, but the
+                  majority have suffered alteration in some form
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* slider - 7  */}
+          <div className={`${classes.slide} allSlider`}>
+            <img src={sliderSevenImg} />
+            <div className={classes.sideContentWrap}>
+              <div className={classes.slideContent}>
+                <h1>Clothing Swap</h1>
                 <p>
                   There are many variations of passages of available, but the
                   majority have suffered alteration in some form
@@ -81,8 +123,15 @@ const Banner = () => {
           </div>
         </div>
         <div className={classes.sliderButton}>
-          <button onClick={prevSlide}>prev</button>
-          <button onClick={nextSlide}>next</button>
+          {buttons?.map((btn, index) => (
+            <button
+              key={index}
+              onClick={() => setCounter(index)}
+              className={index === counter ? classes.active : undefined}
+            >
+              {index + 1}
+            </button>
+          ))}
         </div>
       </div>
       {/* </Container> */}

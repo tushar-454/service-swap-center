13fabe48c268775318a1b7e311df1fd46a081637
tushar-454 <imtushar454@gmail.com>
2023-11-07 11:29:18 +0600

    my schedules 2nd part completed so User can see pending work that services who user added  and which service booked customer

--- src/Components/MySchedules/MySchedules.jsx
+++ src/Components/MySchedules/MySchedules.jsx
@@ -7,11 +7,15 @@ import classes from './MySchedules.module.css';
 
 const MySchedules = () => {
   const [myBooked, setMybooked] = useState([]);
+  const [myPendingWork, setMyPendingWork] = useState([]);
   const { user } = useContext(AuthContext);
   useEffect(() => {
     axios
       .get(`/booking?email=${user?.email}&type=whoBooked`)
       .then((res) => setMybooked(res.data));
+    axios
+      .get(`/booking?email=${user?.email}&type=email`)
+      .then((res) => setMyPendingWork(res.data));
   }, [user.email]);
   return (
     <section>
@@ -58,6 +62,52 @@ const MySchedules = () => {
         </div>
         <div className={classes.sectionTitle}>
           <h1>My Pending Work</h1>
+        </div>
+        {myPendingWork?.length === 0 && (
+          <div className={classes.noFound}>
+            <p>You have not reaming any pending services</p>
+          </div>
+        )}
+        <div className={classes.bookingServicesWrap}>
+          {myPendingWork.length > 0 &&
+            myPendingWork?.map((pendingItem, index) => (
+              <div key={index} className={classes.bookingServicesItem}>
+                <img src={pendingItem.image} alt='services img' />
+                <div className={classes.bookingServicesItemContent}>
+                  <h1>{pendingItem.name}</h1>
+
+                  <p>
+                    <b>Description</b>
+                    {pendingItem.description}
+                  </p>
+                  <p>
+                    <b>Price:</b> {pendingItem.price}
+                  </p>
+                </div>
+
+                <div className={classes.servicesAuthorWrap}>
+                  <div className={classes.servicesAuthor}>
+                    <img src={pendingItem.customerImg} alt='CustomerImg' />
+                    <h3>{pendingItem.customerName}</h3>
+                    <p>Take your service at 9/12/2023 from london</p>
+                  </div>
+                </div>
+
+                <div className={classes.servicesAuthorWrap}>
+                  <div className={classes.servicesAuthor}>
+                    <img src={pendingItem.authorImage} alt='authorimg' />
+                    <h3>{pendingItem.authorName}</h3>
+                  </div>
+                  <div className={classes.viewMore}>
+                    <select name='' id=''>
+                      <option value='Pending'>Pending</option>
+                      <option value='In-Progress'>In-Progress</option>
+                      <option value='Completed'>Completed</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </Container>
     </section>
